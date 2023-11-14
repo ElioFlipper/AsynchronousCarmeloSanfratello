dd98ec09a183600c7d2b63b5ebbc0f658bd3168b
Elio <elio.sanfratello@gmail.com>
2023-11-14 12:39:03 +0100

    network requests 2

---
+++ network2.js
@@ -0,0 +1,30 @@
+// Using async and await, implement the necessary code to perform an HTTP POST request. 
+// To create a new post, use the following URL: link (Method: Post). 
+// The body of the request must contain a post object with the following information: title and Flag Completed. 
+// The post data must be transmitted following the compilation of a form.
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//         title: "Elio ce l'ha fatta",
+//         completed: true,
+//     })
+// })
+
+async function postData(url) {
+    let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            title: "Elio ce l'ha fatta",
+            completed: true,
+        })
+    });
+    let data = await response.json();
+    return data
+}
+
+postData("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => console.log(response))
+    // .then((json) => console.log(json))
